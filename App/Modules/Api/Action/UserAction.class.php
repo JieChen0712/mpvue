1f8159e8e49522e5023c4713b2f43267db6655df
ilyzbs <891878269@qq.com>
2018-05-24 17:48:22 +0800

    注册功能完成

--- App/Modules/Api/Action/UserAction.class.php
+++ App/Modules/Api/Action/UserAction.class.php
@@ -6,10 +6,12 @@
 class UserAction extends CommonAction {
     private $wxapi_obj;
     private $store_obj;
+    private $user_model;
     public function _initialize() {
         import('Lib.Action.Wxapi', 'App');
         import('Lib.Action.Store', 'App');
         $this->store_obj = new Store();
+        $this->user_model = M('user');
         $store = $this->store_obj->get_store([], ['id' => $this->store_id]);//获取店铺信息
         $appid = $store['$list'][0]['appid'];
         $appsecret = $store['$list'][0]['appsecret'];
@@ -25,11 +27,9 @@ class UserAction extends CommonAction {
         }
         //请求接口参数
         $request_data = [
-            'store_id' => $this->store_id,
             'code' => trim(I('code')),//微信code
             'nickname' => trim(I('nickname')),
             'headimgurl' => trim(I('headimgurl')),
-            'sex' => trim(I('sex')),
             'province' => trim(I('province')),
             'city' => trim(I('city')),
             
@@ -39,65 +39,47 @@ class UserAction extends CommonAction {
         if ($wx_result['code'] != 1) {
             $this->ajaxReturn($wx_result);
         }
+        $openid = $wx_result['info']['openid'];
+        $request_data['openid'] = $openid;
         //成功获取opend则判断有没有注册
-        
-        $condition = [
-            'store_id' => $request_data['store_id'],
-            'status' => $this->coupons_obj->status_yes,
-        ];
-        $page_info = array(
-            'page_num' => $request_data['page'],
-        );
-        
-        //获取被领取过的优惠券
-        $coupons_ids = [];//被领取过的ids
-        $coupons_records = $this->coupons_records_model->where(['uid'=>$this->uid, 'store_id'=>$request_data['store_id']])->select();
-        foreach ($coupons_records as $records) {
-            $coupons_ids[] = $records['$coupons_id'];
+        $user = $this->user_model->where(['openid' => $openid])->find();
+        if ($user) {
+            //已经注册
+            $this->ajaxReturn($wx_result);
+        } else {
+            //注册
+            $wx_result = $this->register($request_data);
+            $this->ajaxReturn($wx_result);
         }
-        $result = $this->coupons_obj->get_coupons($page_info, $condition, $coupons_ids);
-        $this->ajaxReturn($result);
     }
     
-    //获取优惠券api
-    public function get_coupons_records()
-    {
-        if( !IS_AJAX ){
-            return FALSE;
+    //注册
+    public function register($data) {
+        $add = [
+            'openid' => $data['openid'],
+            'nickname' => $data['nickname'],
+            'headimgurl' => $data['headimgurl'],
+            'province' => $data['province'],
+            'city' => $data['city'],
+            'created' => time(),
+        ];
+        $res = $this->user_model->add($add);
+        if ($res) {
+            $return_result = [
+                'code' => 1,
+                'msg' => '注册成功',
+                'info' => [
+                    'openid' => $data['openid'],
+                ],
+            ];
+        } else {
+            setLog('注册失败：'.json_encode($add), 'wxapi');
+            $return_result = [
+                'code' => -3,
+                'msg' => '注册失败',
+            ];
         }
-        //请求接口参数
-        $request_data = [
-            'store_id' => $this->store_id,//店铺id
-            'openid' => $this->openid,//微信openid
-            'page' => trim(I('page')),//页数
-        ];
-        
-        $condition = [
-            'uid' => $this->uid,
-            'store_id' => $request_data['store_id'],
-        ];
-        $page_info = array(
-            'page_num' => $request_data['page'],
-        );
-        $result = $this->coupons_obj->get_coupons_records($page_info, $condition);
-        $this->ajaxReturn($result);
-    }
-    
-    //领取优惠券
-    public function add_coupons()
-    {
-        if( !IS_POST ){
-            return FALSE;
-        }
-        //请求接口参数
-        $request_data = [
-            'openid' => $this->openid,//微信openid
-            'coupons_id' => trim(I('coupons_id')),//优惠券id
-            'store_id' => $this->store_id,//店铺id
-        ];
-        $request_data['uid'] = $this->uid;
-        $result = $this->coupons_obj->add_coupoons($request_data);
-        $this->ajaxReturn($result);
+        return $return_result;
     }
 }
 
